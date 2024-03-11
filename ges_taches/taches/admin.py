c774b3dbe070f66bc0db64adce871c3b121fc569
ODJO <arcadodjo@gmail.com>
2024-03-11 01:06:37 +0100

    com3

--- ges_taches/taches/admin.py
+++ ges_taches/taches/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Task
 
 # Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+  list_display = ("name_task", "des_task", "start_date_task", "statut_task")
+
+admin.site.register(Task, TaskAdmin)
